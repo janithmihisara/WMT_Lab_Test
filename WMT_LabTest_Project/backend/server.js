9b1c1f47c20a7d301bec7e96f5280342566c1d0b
IT24101679 <it24101679@my.sliit.lk>
2026-04-28 19:18:39 +0530

    Fix Build

--- WMT_LabTest_Project/backend/server.js
+++ WMT_LabTest_Project/backend/server.js
@@ -17,7 +17,12 @@ app.get("/", (req, res) => {
 
 app.use("/api/items", itemRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
 
 mongoose
   .connect(process.env.MONGO_URI)
